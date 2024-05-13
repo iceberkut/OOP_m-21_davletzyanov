b4a97a33321590954e51474c6cc47fda9d6b3f55
Demid <2@gmail.com>
2024-05-13 09:19:55 +0500

    abstract_fab_pattern

---
+++ Patterns/abstract_fabric.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+
+class CarFactory(ABC):
+    @abstractmethod
+    def produce_car(self):
+        pass
+
+
+class Car(ABC):
+    @abstractmethod
+    def drive(self):
+        pass
+
+
+class ElectricCarFactory(CarFactory):
+    def produce_car(self):
+        return ElectricCar()
+
+
+class PetrolCarFactory(CarFactory):
+    def produce_car(self):
+        return PetrolCar()
+
+
+class HybridCarFactory(CarFactory):
+    def produce_car(self):
+        return HybridCar()
+
+
+class ElectricCar(Car):
+    def drive(self):
+        print('Driving an electric car')
+
+
+class PetrolCar(Car):
+    def drive(self):
+        print('Driving a petrol car')
+
+
+class HybridCar(Car):
+    def drive(self):
+        print('Driving a hybrid car')
+
+
+electric_factory = ElectricCarFactory()
+petrol_factory = PetrolCarFactory()
+hybrid_factory = HybridCarFactory()
+
+electric_car = electric_factory.produce_car()
+petrol_car = petrol_factory.produce_car()
+hybrid_car = hybrid_factory.produce_car()
+
+electric_car.drive()
+# Output: Driving an electric car.
+petrol_car.drive()
+# Output: Driving a petrol car.
+hybrid_car.drive()
+# Output: Driving a hybrid car.
